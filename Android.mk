9210602ebf3cb205caf75c31a4a6d6ee02da2596
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-20 14:37:58 +0200

    QX1050: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: fx_tec_pro1x Model: QX1050 Script version: 2.3.0

---
+++ Android.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),QX1050)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_QX1050.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_QX1050-user \
+    omni_QX1050-userdebug \
+    omni_QX1050-eng
